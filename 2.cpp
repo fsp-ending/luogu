015ead952c69af8f1b133754905899a356c75ee9
fsp-ending <1875346431@qq.com>
2020-05-08 19:20:38 +0800

    2 luogu

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 2.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年05月08日 星期五 19时18分09秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main() {
+    cout << "hello" << endl;
+    return 0;
+}
